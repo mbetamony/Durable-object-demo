8b8631b40cc6f21f73af77e877d0fbf63eb08e56
Mohammad Betamony <mbetamony@atypon.com>
2024-06-06 17:06:01 +0300

    format

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { DurableObject } from "cloudflare:workers"
+import { DurableObject } from 'cloudflare:workers'
 
 /**
  * Welcome to Cloudflare Workers! This is your first Durable Objects application.
@@ -13,14 +13,12 @@ import { DurableObject } from "cloudflare:workers"
  * Learn more at https://developers.cloudflare.com/durable-objects
  */
 
-
 /**
  * Associate bindings declared in wrangler.toml with the TypeScript type system
  */
 export interface Env {
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	MY_DURABLE_OBJECT: DurableObjectNamespace<MyDurableObject>
-
 }
 
 /** A Durable Object's behavior is defined in an exported Javascript class */
@@ -39,105 +37,105 @@ export class MyDurableObject extends DurableObject {
 	async fetch(request: Request): Promise<Response> {
 		const url = new URL(request.url)
 		if (url.pathname.endsWith('steps')) {
-            return await this.handleStepsRequest(request)
-        } else {
-            return await this.handleListenRequest(request)
-        }
+			return await this.handleStepsRequest(request)
+		} else {
+			return await this.handleListenRequest(request)
+		}
 	}
 
 	async handleListenRequest(request: Request) {
-        if (request.headers.get("Upgrade") != "websocket") {
-            return new Response("expected websocket", { status: 400 })
-        }
-        let pair = new WebSocketPair()
-        this.ctx.acceptWebSocket(pair[1])
-        return new Response(null, { status: 101, webSocket: pair[0] })
-    }
+		if (request.headers.get('Upgrade') != 'websocket') {
+			return new Response('expected websocket', { status: 400 })
+		}
+		let pair = new WebSocketPair()
+		this.ctx.acceptWebSocket(pair[1])
+		return new Response(null, { status: 101, webSocket: pair[0] })
+	}
 
 	async handleStepsRequest(request: Request) {
-        const newUrl = new URL(request.url)
+		const newUrl = new URL(request.url)
 		// we don't need those, unless its running locally
-        newUrl.host = 'mbetamony:3000'
-        newUrl.protocol = 'http:'
-        const newReq = new Request(newUrl, request)
+		newUrl.host = 'mbetamony:3000'
+		newUrl.protocol = 'http:'
+		const newReq = new Request(newUrl, request)
 
-        const apiResponse = await fetch(newReq)
-        switch (apiResponse.status) {
-            case 200:
-                const clone = apiResponse.clone()
-                const data = await clone.json()
-                this.broadcast(data as string)
-            default:
-                return apiResponse
-        }
-    }
-	broadcast( message: (ArrayBuffer | ArrayBufferView) | string) {
+		const apiResponse = await fetch(newReq)
+		switch (apiResponse.status) {
+			case 200:
+				const clone = apiResponse.clone()
+				const data = await clone.json()
+				this.broadcast(data as string)
+			default:
+				return apiResponse
+		}
+	}
+	broadcast(message: (ArrayBuffer | ArrayBufferView) | string) {
 		console.log('broadcasting')
-        if (typeof message !== "string") {
-            message = JSON.stringify(message)
-        }
-        this.ctx.getWebSockets().forEach(socket => {
-            try {
-                socket.send(message as string)
-            } catch (err) {
-                this.closeOrErrorHandler(socket)
-            }
-        })
-    }
-	async webSocketMessage(webSocket: WebSocket,  message: string) {
-        const data = JSON.parse(message)
-		if(data.projectID && data.manuscriptID && data.authToken) {	
+		if (typeof message !== 'string') {
+			message = JSON.stringify(message)
+		}
+		this.ctx.getWebSockets().forEach((socket) => {
+			try {
+				socket.send(message as string)
+			} catch (err) {
+				this.closeOrErrorHandler(socket)
+			}
+		})
+	}
+	async webSocketMessage(webSocket: WebSocket, message: string) {
+		const data = JSON.parse(message)
+		if (data.projectID && data.manuscriptID && data.authToken) {
 			const res = await this.fetchListen(data.projectID, data.manuscriptID, data.authToken)
 			webSocket.send(JSON.stringify(res))
 		}
-    }
+	}
 
 	async fetchListen(projectID: string, documentID: string, token: string) {
 		// for the listen, we can't simply fetch the request the same way we do it for the steps request
 		// the listen is initiated on webSockets, and projectID, documentID and token are sent from the websocket
-        const res = await fetch(`http://mbetamony:3000/api/v2/doc/${projectID}/manuscript/${documentID}/listen`, {
-            headers: this.headers(token),
-        })
-        try {
-            return await res.json()
-        } catch (error) {
-            console.log(error)
-            return ''
-        }
-    }
+		const res = await fetch(`http://mbetamony:3000/api/v2/doc/${projectID}/manuscript/${documentID}/listen`, {
+			headers: this.headers(token),
+		})
+		try {
+			return await res.json()
+		} catch (error) {
+			console.log(error)
+			return ''
+		}
+	}
 	private headers(token: string) {
-        return {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-        }
-    }
+		return {
+			Authorization: `Bearer ${token}`,
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+		}
+	}
 	async closeOrErrorHandler(webSocket: WebSocket) {
 		try {
 			webSocket.close()
 		} catch (err) {
 			console.log('error closing socket: ' + err)
 		}
-    }
+	}
 	async webSocketError(webSocket: WebSocket, error: string) {
-        console.log('socket error: ' + error)
-        this.closeOrErrorHandler(webSocket)
-      }
+		console.log('socket error: ' + error)
+		this.closeOrErrorHandler(webSocket)
+	}
 }
 async function runWithErrorHandling(request: Request, func: Function) {
-    try {
-        return await func()
-    } catch (err: any) {
-        if (request.headers.get("Upgrade") == "websocket") {
-            let pair = new WebSocketPair()
-            pair[1].accept()
-            pair[1].send(JSON.stringify({ error: err.stack }))
-            pair[1].close(1011, "Uncaught exception during session setup")
-            return new Response(null, { status: 101, webSocket: pair[0] })
-        } else {
-            return new Response(err.stack, { status: 500 })
-        }
-    }
+	try {
+		return await func()
+	} catch (err: any) {
+		if (request.headers.get('Upgrade') == 'websocket') {
+			let pair = new WebSocketPair()
+			pair[1].accept()
+			pair[1].send(JSON.stringify({ error: err.stack }))
+			pair[1].close(1011, 'Uncaught exception during session setup')
+			return new Response(null, { status: 101, webSocket: pair[0] })
+		} else {
+			return new Response(err.stack, { status: 500 })
+		}
+	}
 }
 export default {
 	/**
